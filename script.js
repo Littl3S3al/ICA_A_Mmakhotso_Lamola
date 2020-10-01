33f8e1b0d098a579dfd2830286dc38efa52dd7e3
Littl3S3al <lauseal@gmail.com>
2020-10-01 15:28:56 +0200

    world script update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r119/build/three.module.js';
 import {OrbitControls} from 'https://threejsfundamentals.org/threejs/resources/threejs/r119/examples/jsm/controls/OrbitControls.js';
 
-import {secondary} from '/wordScript.js';
+import {secondary} from './worldScript.js';
 
 
 
