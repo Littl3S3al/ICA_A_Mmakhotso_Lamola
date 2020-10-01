2ba8b978ef9294d9baf7b17f1577c9d902a5737d
Littl3S3al <lauseal@gmail.com>
2020-10-01 08:46:08 +0200

    raycasting, color change and audio play

--- script.js
+++ script.js
@@ -10,12 +10,19 @@ const threeJsWindow = document.querySelector('#three-js-container');
 const popupWindow = document.querySelector('.popup-window');
 const closeBtn = document.querySelector('#btn-close');
 
+const audios = document.querySelectorAll('audio');
+
+let currentObject;
+
 // loader
 const loadingElem = document.querySelector('#loading');
 const progressBarElem = loadingElem.querySelector('.progressbar');
 
 let windowHalfX = window.innerWidth / 2;
 let windowHalfY = window.innerHeight / 2;
+
+let orbiting = false;
+
 
 // three.js functions
 const main  = () => {
@@ -37,8 +44,8 @@ const main  = () => {
 
     // scene
     const scene = new THREE.Scene();
-    scene.background = new THREE.Color( 0xffffff);
-    scene.fog = new THREE.FogExp2( 0xFFFFFF  , 0.004 );
+    scene.background = new THREE.Color( 0xFCE4EC  );
+    scene.fog = new THREE.FogExp2( 0xFCE4EC  , 0.007 );
 
     
 
@@ -47,18 +54,22 @@ const main  = () => {
     const textureLoader = new THREE.TextureLoader(loadManager);
 
     
-      {
-        const color = 0xFFFFFF;
-        const intensity = 0.8;
+    const addPointLight = (shade, intense, parent, angle, far, top, distance) => {
+        const color = shade;
+        const intensity = intense;
         const light = new THREE.SpotLight(color, intensity);
         light.castShadow = true;
-        light.position.set(0, 100, 0);
+        light.position.set(0, top, 0);
         light.target.position.set(-4, 0, -4);
         light.penumbra = 1;
-        light.angle = 10;
-        scene.add(light);
-        scene.add(light.target);
+        light.angle = angle;
+        light.far = far;
+        light.distance = distance;
+        parent.add(light);
+        parent.add(light.target);
     }
+
+    addPointLight(0xFFFFFF, 0.8, scene, 5, 500, 100, 1000);
 
 
 
@@ -85,7 +96,7 @@ const main  = () => {
 
 
     const CenterOrb = new THREE.Mesh (geometry, invisMaterial)
-    CenterOrb.position.x = 600;
+    CenterOrb.position.x = 300;
     CenterOrb.position.y = 0;
     CenterOrb.position.z = 0;
     scene.add( CenterOrb );
@@ -95,27 +106,29 @@ const main  = () => {
     // sound beacons (pyramids)
 
     const reflectWorld = new THREE.CubeTextureLoader()
-					.setPath( 'assets/soundReflection/' )
-                    .load( [ 'px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png' ] );
+        .setPath( 'assets/soundReflection/' )
+        .load( [ 'px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png' ] );
                     
-    const reflectiveMaterial = new THREE.MeshBasicMaterial( { color: 0xffffff, envMap: reflectWorld} );
+    const reflectiveMaterial = new THREE.MeshBasicMaterial( { color: 'rgb(255,255,255)', envMap: reflectWorld} );
 
-    for ( var i = 0; i < 1; i ++ ) {
+    let soundBeacons = [];
+
+    for ( var i = 0; i < 4; i ++ ) {
         let pos = [
-            {x: -600, z: -50}
+            {x: -200, z: -25}, {x: 250, z: -250}, {x: 200, z: 100}, {x: -250, z: 250}
         ]
         var mesh = new THREE.Mesh( geometry, reflectiveMaterial );
         mesh.position.x = pos[i].x;
         mesh.position.y = 30;
         mesh.position.z = pos[i].z;
-        mesh.updateMatrix();
-        mesh.matrixAutoUpdate = false;
         mesh.castShadow = true; //default is false
         mesh.receiveShadow = true;
+        mesh.name = 'soundBeacon' + i;
         CenterOrb.add( mesh );
+        soundBeacons.push(mesh);
 
     }
-   
+
 
     // set up ground plane
 
@@ -150,6 +163,35 @@ const main  = () => {
         progressBarElem.style.width = progress + '%';
     };
 
+
+    
+  class PickHelper {
+    constructor() {
+      this.raycaster = new THREE.Raycaster();
+      this.pickedObject = null;
+      this.pickedObjectSavedColor = 0;
+    }
+    pick(normalizedPosition, scene, camera, time) {
+      // restore the color if there is a picked object
+      if (this.pickedObject) {
+        this.pickedObject = undefined;
+      }
+
+      // cast a ray through the frustum
+      this.raycaster.setFromCamera(normalizedPosition, camera);
+      // get the list of objects the ray intersected
+      const intersectedObjects = this.raycaster.intersectObjects(CenterOrb.children);
+      if (intersectedObjects.length) {
+        // pick the first object. It's the closest one
+        this.pickedObject = intersectedObjects[0].object;
+      }
+    }
+  }
+
+  const pickPosition = {x: 0, y: 0};
+  const pickHelper = new PickHelper();
+  clearPickPosition();
+
     
 
     renderer.render( scene, camera );
@@ -169,27 +211,110 @@ const main  = () => {
 
     const render = (time) => {
 
+        let itemSelected = false;
+
         time *= 0.00005;
 
         window.addEventListener('resize', onWindowResize, false)
 
-        // CenterOrb.rotation.y = time;
+        CenterOrb.rotation.y = time;
 
-        renderer.outputEncoding = THREE.sRGBEncoding;
-        renderer.toneMapping = THREE.ACESFilmicToneMapping;
-        renderer.toneMappingExposure = 0.2;
+        pickHelper.pick(pickPosition, scene, camera, time);
+        
+        if(pickHelper.pickedObject && !orbiting){
+            if(pickHelper.pickedObject.name){
+                currentObject = pickHelper.pickedObject.name;
+                itemSelected = true;
+                redColor(pickHelper.pickedObject, true);
+            }
+        }
 
-        requestAnimationFrame(render);
-        renderer.setPixelRatio( window.devicePixelRatio );
-        controls.update();  
+        soundBeacons.forEach(beacon => {
+            beacon.rotation.y = time;
+            if(!itemSelected){
+                redColor(beacon, false);
+            }
+        });
+
+
+
+        
+        // renderer.setPixelRatio( window.devicePixelRatio );
+        // controls.update();  
 
         renderer.render(scene, camera);
+        requestAnimationFrame(render);
 
         
     }
 
     requestAnimationFrame(render);
     controls.update();
+
+    const redColor = (object, red) => {
+        let g = object.material.color.g;
+        let b = object.material.color.b;
+        if( g < 1 && !red){ g += 0.01 };
+        if( b < 1 && !red){ b += 0.01 };
+        if( g > 0 && red){ g -= 0.01 };
+        if( b > 0 && red){ b -= 0.01 };
+        object.material.color.setRGB(1, g, b);
+    }
+
+    function getCanvasRelativePosition(event) {
+		const rect = canvas.getBoundingClientRect();
+		return {
+		x: (event.clientX - rect.left) * canvas.width  / rect.width,
+		y: (event.clientY - rect.top ) * canvas.height / rect.height,
+		};
+	}
+
+	function setPickPosition(event) {
+		const pos = getCanvasRelativePosition(event);
+		pickPosition.x = (pos.x /  canvas.width ) *  2 - 1;
+        pickPosition.y = (pos.y / canvas.height) * -2 + 1;  // note we flip Y
+	}
+
+	
+    controls.addEventListener('change', () => {
+
+        orbiting = true;
+
+    });
+
+	function clearPickPosition() {
+		// unlike the mouse which always has a position
+		// if the user stops touching the screen we want
+		// to stop picking. For now we just pick a value
+		// unlikely to pick something
+		pickPosition.x = -100000;
+		pickPosition.y = -100000;
+  }
+  
+
+    window.addEventListener('mousemove', setPickPosition);
+	window.addEventListener('mouseout', clearPickPosition);
+    window.addEventListener('mouseleave', clearPickPosition);
+    window.addEventListener('mouseup', () => {
+        orbiting = false;
+    })
+
+
+	window.addEventListener('touchstart', (event) => {
+		// prevent the window from scrolling
+		event.preventDefault();
+        setPickPosition(event.touches[0]);
+        checkForClick();
+	}, {passive: false});
+
+	window.addEventListener('touchmove', (event) => {
+		setPickPosition(event.touches[0]);
+	});
+
+	window.addEventListener('touchend', () => {
+        clearPickPosition();
+        orbiting = false;
+	})
 }
 
 
@@ -206,3 +331,24 @@ closeBtn.addEventListener('click', () => {
         popupWindow.style.display = 'none';
     }, 1000);
 })
+
+
+// functions
+window.addEventListener('mousedown', () => {
+    checkForClick();
+});
+
+const checkForClick = () => {
+    if(!orbiting && currentObject){
+        var lastChar = currentObject[currentObject.length -1];
+        if(currentObject.includes('soundBeacon')){playSound(lastChar);}
+    }
+
+    currentObject = undefined;
+}
+
+const playSound = (number) => {
+    let no = parseInt(number);
+    audios.forEach(audio => { audio.pause(); })
+    audios[no].play();
+}
